d5ea727c001b4be7214ad0f169a0e620ea374db4
t-nishimura <hbvcg00@gmail.com>
2022-11-11 10:59:54 +0900

    lint

--- include/icp_matching/icp_matching_component.hpp
+++ include/icp_matching/icp_matching_component.hpp
@@ -33,51 +33,51 @@
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <visualization_msgs/msg/marker.hpp>
 // Headers needed in pub/sub, exposed types
-#include <memory> // shared_ptr in pub_
+#include <memory>  // shared_ptr in pub_
 
 namespace icp_matching
 {
-  class IcpMatchingComponent : public rclcpp::Node
-  {
-  public:
-    ICP_MATCHING_ICP_MATCHING_COMPONENT_PUBLIC
-    explicit IcpMatchingComponent(const rclcpp::NodeOptions &options);
+class IcpMatchingComponent : public rclcpp::Node
+{
+public:
+  ICP_MATCHING_ICP_MATCHING_COMPONENT_PUBLIC
+  explicit IcpMatchingComponent(const rclcpp::NodeOptions & options);
 
-  private:
-    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr Scansubscription_;
-    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr Odomsubscription_;
-    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr Posepublisher_;
-    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr OccupancyGridpublisher_;
-    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr MarkerPublisher_;
-    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+private:
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr Scansubscription_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr Odomsubscription_;
+  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr Posepublisher_;
+  rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr OccupancyGridpublisher_;
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr MarkerPublisher_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    nav_msgs::msg::Odometry::SharedPtr odom_;
-    std::vector<int8_t> probability_map_data;
+  rclcpp::TimerBase::SharedPtr timer_;
+  nav_msgs::msg::Odometry::SharedPtr odom_;
+  std::vector<int8_t> probability_map_data;
 
-    const float world_width = 100.f;                      // [m]
-    const float world_height = 100.f;                     // [m]
-    const float map_resolution = 0.05;                    // [m/cell]
-    const int map_width = world_width / map_resolution;   // [cell]
-    const int map_height = world_height / map_resolution; // [cell]
-    const int unOccupied = 0;
-    const int occupied = 100;
-    const int unknown = -1;
-    std::string map_frame = "map";
-    std::string laser_frame = "lidar_link";
+  const float world_width = 100.f;                       // [m]
+  const float world_height = 100.f;                      // [m]
+  const float map_resolution = 0.05;                     // [m/cell]
+  const int map_width = world_width / map_resolution;    // [cell]
+  const int map_height = world_height / map_resolution;  // [cell]
+  const int unOccupied = 0;
+  const int occupied = 100;
+  const int unknown = -1;
+  std::string map_frame = "map";
+  std::string laser_frame = "lidar_link";
 
-    void Odom_topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
-    void Scan_topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
+  void Odom_topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void Scan_topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg);
 
-    /**
+  /**
      * @brief 点群の点の間隔を一定に揃える
      * 実装についてはゼロから始めるSLAM入門のP41を参照
      * @param vec
      */
-    void resamplePoints(std::vector<geometry_msgs::msg::Point> &vec);
+  void resamplePoints(std::vector<geometry_msgs::msg::Point> & vec);
 
-    /**
+  /**
      * @brief 点群の位置とロボットの位置の間を結んだ直線上のPixelをすべて
      * UnOccupiedで塗りつぶす
      * @param x1
@@ -85,9 +85,9 @@ namespace icp_matching
      * @param y1
      * @param y2
      */
-    void plotBresenhamLine(int x1, int x2, int y1, int y2);
-    void publishMap();
-    void publishMarker(std::vector<geometry_msgs::msg::Point> &vec);
-    int getRasterScanIndex(int width, int x, int y) { return y * width + x; }
-  };
-} // namespace icp_matching
+  void plotBresenhamLine(int x1, int x2, int y1, int y2);
+  void publishMap();
+  void publishMarker(std::vector<geometry_msgs::msg::Point> & vec);
+  int getRasterScanIndex(int width, int x, int y) { return y * width + x; }
+};
+}  // namespace icp_matching
--- src/icp_matching_component.cpp
+++ src/icp_matching_component.cpp
@@ -21,201 +21,186 @@
 
 namespace icp_matching
 {
-    IcpMatchingComponent::IcpMatchingComponent(const rclcpp::NodeOptions &options)
-        : Node("icp_matching_node", options)
-    {
-        probability_map_data.resize(map_height * map_width);
-        std::fill(probability_map_data.begin(), probability_map_data.end(), unknown);
-        Odomsubscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-            "/odom", 10,
-            std::bind(&IcpMatchingComponent::Odom_topic_callback, this, std::placeholders::_1));
-        Scansubscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-            "/scan", rclcpp::QoS(10).best_effort().durability_volatile(),
-            std::bind(&IcpMatchingComponent::Scan_topic_callback, this, std::placeholders::_1));
-        OccupancyGridpublisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/map", 1);
-        MarkerPublisher_ = this->create_publisher<visualization_msgs::msg::Marker>("/marker", 10);
-        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+IcpMatchingComponent::IcpMatchingComponent(const rclcpp::NodeOptions & options)
+: Node("icp_matching_node", options)
+{
+  probability_map_data.resize(map_height * map_width);
+  std::fill(probability_map_data.begin(), probability_map_data.end(), unknown);
+  Odomsubscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    "/odom", 10,
+    std::bind(&IcpMatchingComponent::Odom_topic_callback, this, std::placeholders::_1));
+  Scansubscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+    "/scan", rclcpp::QoS(10).best_effort().durability_volatile(),
+    std::bind(&IcpMatchingComponent::Scan_topic_callback, this, std::placeholders::_1));
+  OccupancyGridpublisher_ = this->create_publisher<nav_msgs::msg::OccupancyGrid>("/map", 1);
+  MarkerPublisher_ = this->create_publisher<visualization_msgs::msg::Marker>("/marker", 10);
+  tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-        using namespace std::chrono_literals;
-        // 1秒ごとにOccupancyGridMapをpublishする
-        timer_ = this->create_wall_timer(1s, std::bind(&IcpMatchingComponent::publishMap, this));
+  using namespace std::chrono_literals;
+  // 1秒ごとにOccupancyGridMapをpublishする
+  timer_ = this->create_wall_timer(1s, std::bind(&IcpMatchingComponent::publishMap, this));
+}
+
+void IcpMatchingComponent::Scan_topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+  try {
+    std::vector<geometry_msgs::msg::Point> point_vec;
+    geometry_msgs::msg::TransformStamped laserToMap =
+      tf_buffer_->lookupTransform(map_frame, laser_frame, tf2::TimePointZero);
+    tf2::Quaternion q(
+      laserToMap.transform.rotation.x, laserToMap.transform.rotation.y,
+      laserToMap.transform.rotation.z, laserToMap.transform.rotation.w);
+    tf2::Matrix3x3 m(q);
+    double roll, pitch, yaw;
+    m.getRPY(roll, pitch, yaw);
+    float current_angle = msg->angle_min;
+    for (float & scan : msg->ranges) {
+      if (msg->range_min <= scan && scan <= msg->range_max) {
+        geometry_msgs::msg::Point point;
+        // 極座標系をlaserのx,yに変換
+        point.x =
+          scan * cos(current_angle + yaw) + laserToMap.transform.translation.x + world_width * 0.5f;
+        point.y = scan * sin(current_angle + yaw) + laserToMap.transform.translation.y +
+                  world_height * 0.5f;
+        point_vec.emplace_back(point);
+        current_angle += msg->angle_increment;
+      }
     }
-
-    void IcpMatchingComponent::Scan_topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
-    {
-        try
-        {
-            std::vector<geometry_msgs::msg::Point> point_vec;
-            geometry_msgs::msg::TransformStamped laserToMap =
-                tf_buffer_->lookupTransform(map_frame, laser_frame, tf2::TimePointZero);
-            tf2::Quaternion q(
-                laserToMap.transform.rotation.x, laserToMap.transform.rotation.y, laserToMap.transform.rotation.z, laserToMap.transform.rotation.w);
-            tf2::Matrix3x3 m(q);
-            double roll, pitch, yaw;
-            m.getRPY(roll, pitch, yaw);
-            float current_angle = msg->angle_min;
-            for (float &scan : msg->ranges)
-            {
-                if (msg->range_min <= scan && scan <= msg->range_max)
-                {
-                    geometry_msgs::msg::Point point;
-                    // 極座標系をlaserのx,yに変換
-                    point.x = scan * cos(current_angle + yaw) + laserToMap.transform.translation.x + world_width * 0.5f;
-                    point.y = scan * sin(current_angle + yaw) + laserToMap.transform.translation.y + world_height * 0.5f;
-                    point_vec.emplace_back(point);
-                    current_angle += msg->angle_increment;
-                }
-            }
-            geometry_msgs::msg::TransformStamped mapToBaseLink =
-                tf_buffer_->lookupTransform("base_link", map_frame, tf2::TimePointZero);
-            const int cell_robot_x = floor((mapToBaseLink.transform.translation.x + world_width * 0.5f) / map_resolution);
-            const int cell_robot_y = floor((mapToBaseLink.transform.translation.y + world_height * 0.5f) / map_resolution);
-            for (geometry_msgs::msg::Point &point : point_vec)
-            {
-                const int cell_point_x = floor(point.x / map_resolution);
-                const int cell_point_y = floor(point.y / map_resolution);
-                plotBresenhamLine(cell_point_x, cell_robot_x, cell_point_y, cell_robot_y);
-                probability_map_data[getRasterScanIndex(map_width, cell_point_x, cell_point_y)] = occupied;
-            }
-            // publishMarker(point_vec);
-        }
-        catch (const tf2::TransformException &ex)
-        {
-            RCLCPP_INFO(get_logger(), "Could not transform %s to %s: %s", laser_frame, map_frame, ex.what());
-            return;
-        }
+    geometry_msgs::msg::TransformStamped mapToBaseLink =
+      tf_buffer_->lookupTransform("base_link", map_frame, tf2::TimePointZero);
+    const int cell_robot_x =
+      floor((mapToBaseLink.transform.translation.x + world_width * 0.5f) / map_resolution);
+    const int cell_robot_y =
+      floor((mapToBaseLink.transform.translation.y + world_height * 0.5f) / map_resolution);
+    for (geometry_msgs::msg::Point & point : point_vec) {
+      const int cell_point_x = floor(point.x / map_resolution);
+      const int cell_point_y = floor(point.y / map_resolution);
+      plotBresenhamLine(cell_point_x, cell_robot_x, cell_point_y, cell_robot_y);
+      probability_map_data[getRasterScanIndex(map_width, cell_point_x, cell_point_y)] = occupied;
     }
+    // publishMarker(point_vec);
+  } catch (const tf2::TransformException & ex) {
+    RCLCPP_INFO(
+      get_logger(), "Could not transform %s to %s: %s", laser_frame, map_frame, ex.what());
+    return;
+  }
+}
 
-    void IcpMatchingComponent::Odom_topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
-    {
-        // RCLCPP_INFO(get_logger(), "x : %f y : %f", msg->pose.pose.position.x, msg->pose.pose.position.y);
+void IcpMatchingComponent::Odom_topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+  // RCLCPP_INFO(get_logger(), "x : %f y : %f", msg->pose.pose.position.x, msg->pose.pose.position.y);
+}
+
+void IcpMatchingComponent::resamplePoints(std::vector<geometry_msgs::msg::Point> & vec)
+{
+  const float interpolate_threthold_min_m = 0.015f;  // [m]
+  const float interpolate_threthold_max_m = 0.02f;   // [m]
+  if (vec.size() == 0) {
+    return;
+  }
+  int index = 0;
+  float distance_sum = 0.f;
+  geometry_msgs::msg::Point last_point = vec.at(index);
+  std::vector<geometry_msgs::msg::Point> interpolated_points;
+  interpolated_points.emplace_back(last_point);
+  while (index < vec.size()) {
+    const geometry_msgs::msg::Point current_point = vec.at(index);
+    const float dx = current_point.x - last_point.x;
+    const float dy = current_point.y - last_point.y;
+    const float distance_between_neighbor_points = sqrt(pow(dx, 2) + pow(dy, 2));
+    // RCLCPP_INFO(get_logger(), "%f", distance_between_neighbor_points);
+    distance_sum += distance_between_neighbor_points;
+    if (distance_sum < interpolate_threthold_min_m) {
+      last_point = current_point;
+      index++;
+    } else if (distance_sum >= interpolate_threthold_max_m) {
+      interpolated_points.emplace_back(current_point);
+      last_point = current_point;
+      distance_sum = 0.f;
+      index++;
+    } else {
+      const float ratio =
+        (distance_between_neighbor_points - (distance_sum - interpolate_threthold_min_m)) /
+        distance_between_neighbor_points;
+      geometry_msgs::msg::Point newPoint;
+      newPoint.x = last_point.x + dx * ratio;
+      newPoint.y = last_point.y + dy * ratio;
+      interpolated_points.emplace_back(newPoint);
     }
+  }
+}
 
-    void IcpMatchingComponent::resamplePoints(std::vector<geometry_msgs::msg::Point> &vec)
-    {
-        const float interpolate_threthold_min_m = 0.015f; // [m]
-        const float interpolate_threthold_max_m = 0.02f;  // [m]
-        if (vec.size() == 0)
-        {
-            return;
-        }
-        int index = 0;
-        float distance_sum = 0.f;
-        geometry_msgs::msg::Point last_point = vec.at(index);
-        std::vector<geometry_msgs::msg::Point> interpolated_points;
-        interpolated_points.emplace_back(last_point);
-        while (index < vec.size())
-        {
-            const geometry_msgs::msg::Point current_point = vec.at(index);
-            const float dx = current_point.x - last_point.x;
-            const float dy = current_point.y - last_point.y;
-            const float distance_between_neighbor_points = sqrt(pow(dx, 2) + pow(dy, 2));
-            // RCLCPP_INFO(get_logger(), "%f", distance_between_neighbor_points);
-            distance_sum += distance_between_neighbor_points;
-            if (distance_sum < interpolate_threthold_min_m)
-            {
-                last_point = current_point;
-                index++;
-            }
-            else if (distance_sum >= interpolate_threthold_max_m)
-            {
-                interpolated_points.emplace_back(current_point);
-                last_point = current_point;
-                distance_sum = 0.f;
-                index++;
-            }
-            else
-            {
-                const float ratio =
-                    (distance_between_neighbor_points - (distance_sum - interpolate_threthold_min_m)) /
-                    distance_between_neighbor_points;
-                geometry_msgs::msg::Point newPoint;
-                newPoint.x = last_point.x + dx * ratio;
-                newPoint.y = last_point.y + dy * ratio;
-                interpolated_points.emplace_back(newPoint);
-            }
-        }
+void IcpMatchingComponent::publishMap()
+{
+  nav_msgs::msg::OccupancyGrid map_;
+  // OccupancyGridの座標系はmap
+  map_.header.frame_id = map_frame;
+  // 時間は現在時刻
+  map_.header.stamp = get_clock()->now();
+  map_.info.width = map_width;
+  map_.info.height = map_height;
+  map_.info.resolution = map_resolution;
+  // originではマップの左下のセル（0,0）の位置を指定する
+  // このように指定するとマップの中心がセルの中心となる
+  map_.info.origin.position.x = -world_width * 0.5f;
+  map_.info.origin.position.y = -world_height * 0.5f;
+  map_.data = probability_map_data;
+  OccupancyGridpublisher_->publish(map_);
+}
+
+void IcpMatchingComponent::publishMarker(std::vector<geometry_msgs::msg::Point> & vec)
+{
+  visualization_msgs::msg::Marker marker;
+  marker.header.frame_id = map_frame;
+  marker.header.stamp = get_clock()->now();
+  marker.action = visualization_msgs::msg::Marker::ADD;
+  marker.id = 0;
+  marker.type = visualization_msgs::msg::Marker::POINTS;
+  marker.points = vec;
+  marker.scale.x = 0.02;
+  marker.scale.y = 0.02;
+  marker.color.g = 1.0f;
+  marker.color.a = 1.0;
+  MarkerPublisher_->publish(marker);
+}
+
+void IcpMatchingComponent::plotBresenhamLine(int x1, int x2, int y1, int y2)
+{
+  const bool steep = abs(y2 - y1) > abs(x2 - x1);
+  if (steep) {
+    using std::swap;
+    swap(x1, y1);
+    swap(x2, y2);
+  }
+  if (x1 > x2) {
+    using std::swap;
+    swap(x1, x2);
+    swap(y1, y2);
+  }
+  const int deltax = x2 - x1;
+  const int deltay = abs(y2 - y1);
+  int error = deltax / 2;
+  int y = y1;
+  const int ystep = y1 < y2 ? 1 : -1;
+  for (int x = x1; x < x2; x++) {
+    int index = 0;
+
+    if (steep) {
+      index = getRasterScanIndex(map_width, y, x);
+    } else {
+      index = getRasterScanIndex(map_width, x, y);
     }
-
-    void IcpMatchingComponent::publishMap()
-    {
-        nav_msgs::msg::OccupancyGrid map_;
-        // OccupancyGridの座標系はmap
-        map_.header.frame_id = map_frame;
-        // 時間は現在時刻
-        map_.header.stamp = get_clock()->now();
-        map_.info.width = map_width;
-        map_.info.height = map_height;
-        map_.info.resolution = map_resolution;
-        // originではマップの左下のセル（0,0）の位置を指定する
-        // このように指定するとマップの中心がセルの中心となる
-        map_.info.origin.position.x = -world_width * 0.5f;
-        map_.info.origin.position.y = -world_height * 0.5f;
-        map_.data = probability_map_data;
-        OccupancyGridpublisher_->publish(map_);
+    if (0 <= index && index < probability_map_data.size()) {
+      probability_map_data.at(index) = unOccupied;
     }
-
-    void IcpMatchingComponent::publishMarker(std::vector<geometry_msgs::msg::Point> &vec)
-    {
-        visualization_msgs::msg::Marker marker;
-        marker.header.frame_id = map_frame;
-        marker.header.stamp = get_clock()->now();
-        marker.action = visualization_msgs::msg::Marker::ADD;
-        marker.id = 0;
-        marker.type = visualization_msgs::msg::Marker::POINTS;
-        marker.points = vec;
-        marker.scale.x = 0.02;
-        marker.scale.y = 0.02;
-        marker.color.g = 1.0f;
-        marker.color.a = 1.0;
-        MarkerPublisher_->publish(marker);
+    error -= deltay;
+    if (error < 0) {
+      y += ystep;
+      error += deltax;
     }
-
-    void IcpMatchingComponent::plotBresenhamLine(int x1, int x2, int y1, int y2)
-    {
-        const bool steep = abs(y2 - y1) > abs(x2 - x1);
-        if (steep)
-        {
-            using std::swap;
-            swap(x1, y1);
-            swap(x2, y2);
-        }
-        if (x1 > x2)
-        {
-            using std::swap;
-            swap(x1, x2);
-            swap(y1, y2);
-        }
-        const int deltax = x2 - x1;
-        const int deltay = abs(y2 - y1);
-        int error = deltax / 2;
-        int y = y1;
-        const int ystep = y1 < y2 ? 1 : -1;
-        for (int x = x1; x < x2; x++)
-        {
-            int index = 0;
-
-            if (steep)
-            {
-                index = getRasterScanIndex(map_width, y, x);
-            }
-            else
-            {
-                index = getRasterScanIndex(map_width, x, y);
-            }
-            if (0 <= index && index < probability_map_data.size())
-            {
-                probability_map_data.at(index) = unOccupied;
-            }
-            error -= deltay;
-            if (error < 0)
-            {
-                y += ystep;
-                error += deltax;
-            }
-        }
-    }
-} // namespace icp_matching
+  }
+}
+}  // namespace icp_matching
 
 RCLCPP_COMPONENTS_REGISTER_NODE(icp_matching::IcpMatchingComponent)
